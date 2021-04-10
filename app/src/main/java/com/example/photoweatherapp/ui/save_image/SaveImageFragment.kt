f348e8bad14e0dcf6fa80d750202d9ce40117bd1
Islam Mostafa <islam.m.mohamed95@gmail.com>
2021-04-10 11:27:25 +0200

    Redesign Weather Banner

--- app/src/main/java/com/example/photoweatherapp/ui/save_image/SaveImageFragment.kt
+++ app/src/main/java/com/example/photoweatherapp/ui/save_image/SaveImageFragment.kt
@@ -2,7 +2,6 @@ package com.example.photoweatherapp.ui.save_image
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,14 +13,10 @@ import com.example.photoweatherapp.R
 import com.example.photoweatherapp.model.WeatherModel
 import com.example.photoweatherapp.utils.saveImageToStorage
 import com.squareup.picasso.Picasso
-import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.fragment_save_image.*
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.io.File
-import java.lang.Exception
 
 class SaveImageFragment : Fragment() {
 
@@ -68,13 +63,13 @@ class SaveImageFragment : Fragment() {
 
     private fun bindData(weatherModel: WeatherModel?, filePath: String) {
         weatherModel.apply {
-            textView.text = this?.weather?.first()?.main
-            textView2.text = this?.main?.tempMax.toString()
-            textView3.text = this?.main?.tempMin.toString()
-            textView4.text = this?.main?.pressure.toString()
-            textView5.text = this?.main?.humidity.toString()
-            textView6.text = this?.wind?.speed.toString()
-            textView7.text = this?.name
+            tv_status.text = this?.weather?.first()?.main
+            tv_max.text = this?.main?.tempMax.toString()+" /"
+            tv_min.text = this?.main?.tempMin.toString()
+            tv_pressure.text = getString(R.string.pressure,this?.main?.pressure.toString())
+            tv_humidity.text = getString(R.string.humidity,this?.main?.humidity.toString())
+            tv_wind_speed.text = getString(R.string.wind_speed,this?.wind?.speed.toString())
+            tv_address.text = this?.name
             Picasso.get().load(File(filePath)).into(imageView2)
         }
     }
@@ -90,7 +85,6 @@ class SaveImageFragment : Fragment() {
             )
             val canvas = Canvas(imageBitmap)
             image_Details?.draw(canvas)
-
             saveImageToStorage(requireContext(), imageBitmap) {
                 lifecycleScope.launch(Dispatchers.Main){
                     val bundle  = Bundle()
